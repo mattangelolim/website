9c92b54466b5af90fc46f279441a2a784250d9d0
Matt Angelo Lim <mattlim248@gmail.com>
2023-07-09 15:56:06 +0800

    changes

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,8 +6,8 @@ import li from "../assets/li.png";
 
 const about = () => {
   return (
-    <div className="content sm:block md:flex">
-      <div className="container md:block md:justify-center md:ml-96 md:items-center sm:block sm:ml-8">
+    <div className="content lg:flex">
+      <div className="container lg:block lg:justify-center lg:ml-96 lg:items-center">
         <img src={pic} alt="pic" className=" rounded-full h-2/5 mx-auto my-4" />
         <h1 className="font-bold text-4xl text-center py-4 md:py-8">
           Matt Lim
@@ -30,18 +30,18 @@ const about = () => {
           />
         </div>
       </div>
-      <div className="container-2 md:text-justify md:ml-10 sm:relative sm:mx-8 sm:text-justify">
+      <div className="container-2 md:text-justify md:ml-10 sm:relative  sm:text-justify">
         <h1 className="md:text-8xl font-semibold sm:text-6xl">Hello</h1>
-        <p className="md:text-4xl md:mt-5 md:mb-5 sm:mt-5 sm:mb-5 sm:text-3xl">
+        <h2 className="md:text-4xl md:mt-5 md:mb-5">
           Here's who I am & what I do
-        </p>
+        </h2>
         <button className="resume text-white bg-blue-700 rounded-lg px-4 py-2 mr-4 hover:bg-slate-50 hover:text-black ease-in-out duration-300">
           Resume
         </button>
         <button className="project rounded-lg px-4 py-2 mr-4 bg-white border hover:bg-blue-700 hover:text-white ease-in-out duration-300">
           Projects
         </button>
-        <p className="md:mt-8 text-xl text-gray-700 sm:mt-8">
+        <p className="md:mt-8 text-xl text-gray-700">
           Passionate computer engineering graduate with a focus on web
           development. Strong foundation in programming, algorithms, and
           software engineering principles. Dedicated to staying current with the
